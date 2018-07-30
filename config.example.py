3702b3cce20123a54756ef78cbc3e4aa6a0401ab
bmintz <bmintz@protonmail.com>
2018-07-29 22:52:25 -0500

    add example config

---
+++ config.example.py
@@ -0,0 +1,17 @@
+{
+	'cogs': (
+		'cogs.emoji',
+		'ben_cogs.debug',
+		'ben_cogs.misc',
+		'jishaku',
+	),
+
+	'tokens': {
+		'discord': 'sek.rit.token',
+		'stats': {
+			'bots.discord.pw': None,
+			'discordbots.org': None,
+			'botsfordiscord.com': None,
+		},
+	},
+}

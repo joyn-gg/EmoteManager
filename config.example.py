a7d9c037864479c2c9951c457ba5345ce8c2bf51
io mintz <io@mintz.cc>
2020-05-12 23:50:15 +0000

    add copyright license file

--- config.example.py
+++ config.example.py
@@ -28,6 +28,8 @@
 		},
 	},
 
+	'copyright_license_file': 'short-license.txt',
+
 	'socks5_proxy_url': None,  # required for connecting to the EC API over a Tor onion service
 	'use_socks5_for_all_connections': False,  # whether to use socks5 for all HTTP operations (other than discord.py)
 	'user_agent': 'EmoteManagerBot (https://github.com/iomintz/emote-manager-bot)',
